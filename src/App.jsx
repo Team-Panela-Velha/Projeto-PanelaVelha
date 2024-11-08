3d251a6a6e047dedd64f58c93a14c63c6751b55d
JoYoneyama <jvsyoneyama@gmail.com>
2024-11-08 12:23:10 -0300

    testando cores e posicao da logo

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ function App() {
   return (
     <>
       <div className="flex bg-stone-50  min-h-screen">
-      <aside className='flex justify-start w-1/5 bg-aside-color shadow-right-only'>
-        <div className='w-80 h-80  border-zinc-950 rounded-full'>
-          <img className="w-full" src="src/assets/img/logo.png" alt="" />
-        </div>
-      </aside>
-      <main className='flex flex-col w-4/5 bg-body-color'>
-        main
-      </main>
+        <aside className='flex justify-start w-1/5 bg-aside-color shadow-right-only border-r-2 border-img-border-color'>
+          <div className='flex justify-center h-1/6 my-8'>
+            <img className="border-2 border-img-border-color rounded-full" src="src/assets/img/logo.png" alt="" />
+          </div>
+        </aside>
+
+        <main className='flex flex-col w-4/5 bg-body-color'>
+          main
+        </main>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ export default {
     extend: {
       colors: {
         "body-color": "#f7efe4",
-        "aside-color": "#c13236"
+        "aside-color": "#c13236",
+        "img-border-color": "#430009"
       },
       boxShadow: {
         'right-only': '4px 0 8px rgba(0, 0, 0, 0.2)', // ajuste o valor conforme necessário
