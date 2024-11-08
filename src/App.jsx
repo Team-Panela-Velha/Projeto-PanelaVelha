0629a15b573b9090043ddc8819be6decdaa7bb99
JoYoneyama <jvsyoneyama@gmail.com>
2024-11-08 12:31:26 -0300

    Adicionando possiveis componentes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <div className="flex bg-stone-50  min-h-screen">
+
         <aside className='flex justify-start w-1/5 bg-aside-color shadow-right-only border-r-2 border-img-border-color'>
           <div className='flex justify-center h-1/6 my-8'>
             <img className="border-2 border-img-border-color rounded-full" src="src/assets/img/logo.png" alt="" />
@@ -15,6 +16,7 @@ function App() {
         <main className='flex flex-col w-4/5 bg-body-color'>
           main
         </main>
+        
       </div>
     </>
   )
