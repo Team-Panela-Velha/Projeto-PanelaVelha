08c99085982e06f402baf84316792bf111840c06
Mathlps <matheussouzalopes2006@gmail.com>
2024-11-11 20:21:07 -0300

    fixed aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Aside from './components/Aside'
+
+import React from 'react';
+import './App.css';
+import Aside from './components/Aside';
 
 function App() {
-  
-  return (
-    <>
-      <div className="flex min-h-screen">
-        <Aside/>
-        <main className='flex flex-col w-5/6 bg-peaches'>
-          <div className="w-full h-screen">
-            <div className='w-[95%] h-[90%] m-5 bg-black '>h</div>
-          </div>
-        </main>
-        
-      </div>
-    </>
-  )
+    return (
+        <div className="flex min-h-screen">
+            <Aside />
+            <main className='flex flex-col w-[80%] ml-[20%] bg-peaches'>
+                <div className="w-full h-screen">
+                    <div className='flex justify-center w-[65%] h-[60%] m-5 bg-black'>h</div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Searchbar from './Searchbar';
+import './Aside.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Aside() {
     return (
         <>
 
-            <aside className='flex flex-col w-1/4 bg-redwood h-screen'>
+            <aside className='aside-fixo flex flex-col w-1/5 bg-redwood h-screen'>
                 <div className='flex justify-center items-center gap-5 pt-6'>
                     <img className="rounded-full w-12 h-12 border-2 border-chocolate-cosmos" src="src/assets/img/logo.png" alt="Logo Panela Velha" />
                     <h1 className='font-black text-jet text-3xl w-1/2'>PANELA VELHA</h1>
@@ -15,7 +16,7 @@ function Aside() {
                 <div className="flex justify-center pt-6 pb-6 w-full">
                     <Searchbar />
                 </div>
-                <nav className='flex justify-start pl-4 w-full'>
+                <nav className='flex justify-start pl-7 w-full'>
                     <ul className='flex flex-col items-start uppercase text-sm font-semibold text-snow'>
                         <li className='pb-2'>home</li>
                         <li className='pb-2'>receitas</li>
@@ -24,7 +25,7 @@ function Aside() {
                         <li className='pb-2'>cadastro</li>
                     </ul>
                 </nav>
-                <div className='flex flex-col justify-end items-start pl-4 pb-5 w-full h-full'>
+                <div className='flex flex-col justify-end items-start pl-7 pb-5 w-full h-full'>
                     <div className='flex'>
                         <i class="bi bi-instagram pr-2 text-jet"></i>
                         <i class="bi bi-github text-jet"></i>
