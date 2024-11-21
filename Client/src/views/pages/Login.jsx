50eb884a60cdbf936bf3ff17ede52b9b976a703c
JoYoneyama <jvsyoneyama@gmail.com>
2024-11-21 16:31:55 -0300

    Adicionando rascunho do axios (n precisa instalar ele ainda)

--- Client/src/views/pages/Login.jsx
+++ Client/src/views/pages/Login.jsx
@@ -1,21 +1,37 @@
 import { Link } from "react-router-dom"
 
 import "../../components/css/Login.css"
+import { useState } from "react";
 
-const Login = () => (
-    <>
+const Login = () => {
+
+    // const [nome, setNome] = useState("");
+    // const [senha, setSenha] = useState("");
+
+    // const enviarDados = async(e) => {
+    //     e.preventDefault();
+
+    //     try{
+    //         const response = await axios.post("http://127.0.0.1:5000/api/login", {nome, senha,});
+    //     } catch (error) {
+    //         console.error("Erro ao enviar dados: ", error);
+    //     }
+    // };
+    
+    return (
+
         <div className="flex justify-center items-center w-full h-screen">
-            <form className="flex flex-col items-center justify-center gap-8 w-2/5 h-3/4 bg-slate-100 rounded-lg tela-login">
+            <form className="flex flex-col items-center justify-center gap-8 w-2/5 h-3/4 bg-slate-100 rounded-lg tela-login" /*onSubmit={enviarDados}*/>
                 <h1 className="text-redwood font-bold text-3xl mb-4">LOGIN</h1>
                 <div className="flex flex-col gap-4 w-full">
                     <div className="flex flex-col gap-1">
                         <label className="relative left-1/3" for="username">Usuário</label>
-                        <input className="w-1/3 relative left-1/3 h-7" type="text" id="username" name="username" required></input>
+                        <input className="w-1/3 relative left-1/3 h-7" type="text" id="username" name="username" /*value={nome} onChange={(e) => setNome(e.target.value)}*/ required></input>
                     </div>
                         
                     <div className="flex flex-col gap-1">
                         <label className="relative left-1/3" for="password">Senha</label>
-                        <input className="w-1/3 relative left-1/3 h-7" type="password" id="password" name="password" required></input>
+                        <input className="w-1/3 relative left-1/3 h-7" type="password" id="password" name="password" /*value={senha} onChange={(e) => setSenha(e.target.value)}*/ required></input>
                     </div>
                 </div>
                 <div className="w-3/12">
@@ -26,7 +42,8 @@ const Login = () => (
                 <p className="relative right-[13%] top-10">Não possui uma conta? <Link to="/cadastro" className="text-redwood">Cadastre-se</Link></p>    
             </form>
         </div>
-    </>
-)
+
+    );
+};
 
 export default Login
