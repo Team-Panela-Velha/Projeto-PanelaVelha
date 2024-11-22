657a88408906599d350139a3a0bb57ef8ed2d31d
Mathlps <matheussouzalopes2006@gmail.com>
2024-11-21 21:57:49 -0300

    upgrade receitas

--- Client/src/views/pages/ReceitaPage.jsx
+++ Client/src/views/pages/ReceitaPage.jsx
@@ -21,27 +21,28 @@ const ReceitaPage = () => (
                             <img className="w-[30rem] h-[20rem]" src="https://octo.com.br/wp-content/uploads/2020/03/Blog_caipirinha_de_limao.jpg" alt="" />
                         </div>
                         <div className="flex justify-between m-2">
-                        <div>
-                            <i class="bi bi-circle-fill"></i> 5 porções
+                            <div className="text-[0.8rem] flex gap-1 justify-center">
+                                <i class="bi bi-circle-fill"></i>
+                                <p>5 porções</p>
                             </div>
-                            <div>
-                                <i class="bi bi-stopwatch-fill"></i> 12min
+                            <div className="flex gap-1 text-[0.8rem] justify-center">
+                                <i class="bi bi-stopwatch-fill"></i> 
+                                <p>12min</p>
                             </div>
-                            <div>
+                            <div className="gap-1 text-[0.8rem] justify-center text-center">
                                 <i class="bi bi-star-fill"></i>
                                 <i class="bi bi-star-fill"></i>
                                 <i class="bi bi-star-fill"></i>
                                 <i class="bi bi-star-fill"></i>
                                 <i class="bi bi-star-fill"></i>
-                                <p className="text-center">Avalie!!</p>
+                                <p>Avalie!!</p>
                             </div>
-                            
                         </div>
                     </div>
                 </div>
                 <div className="flex justify-between mr-3 mt-3">
-                    <div className=" border-r-2">
-                        <h1>ingredientes</h1>
+                    <div className="border-r-2">
+                        <h1>igredientes</h1>
                     </div>
                     <div>
                         modo de preparo
