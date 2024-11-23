e8c3a3efde4b2ffb40d26ebf5e9641942e4cea42
Mathlps <matheussouzalopes2006@gmail.com>
2024-11-23 15:07:24 -0300

    regioes

--- Client/src/views/pages/ReceitaPage.jsx
+++ Client/src/views/pages/ReceitaPage.jsx
@@ -1,4 +1,7 @@
-
+import Garfo from "../../assets/img/garfo-e-faca.png";
+import Relogio from "../../assets/img/despertar.png";
+import Chefe from "../../assets/img/chefe-de-cozinha.png";
+import Nivel from "../../assets/img/elevacao.png";
 const ReceitaPage = () => (
     <>
         <div className="w-full h-screen">
@@ -18,26 +21,38 @@ const ReceitaPage = () => (
                 <div className="flex mt-5 mr-3 bg-red-100">
                     <div className="flex flex-col m-2" >
                         <div>
-                            <img className="w-[30rem] h-[20rem]" src="https://octo.com.br/wp-content/uploads/2020/03/Blog_caipirinha_de_limao.jpg" alt="" />
+                            <img className="w-[30rem] h-[20rem]" src="https://images.pexels.com/photos/27626304/pexels-photo-27626304/free-photo-of-comida-alimento-refeicao-frio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                         </div>
-                        <div className="flex justify-between m-2">
-                            <div className="text-[0.8rem] flex gap-1 justify-center">
-                                <i class="bi bi-circle-fill"></i>
-                                <p>5 porções</p>
-                            </div>
-                            <div className="flex gap-1 text-[0.8rem] justify-center">
-                                <i class="bi bi-stopwatch-fill"></i> 
-                                <p>12min</p>
-                            </div>
-                            <div className="gap-1 text-[0.8rem] justify-center text-center">
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <p>Avalie!!</p>
-                            </div>
+                    </div>
+                    <div className="flex flex-col w-[35rem]">
+                        <div className="w-full">
+                            <img className="w-16 h-16 justify-self-center mt-5 " src={Garfo} alt="" />
                         </div>
+                        <div className="w-full text-xs text-center">
+                            <p className="text-sm font-semibold mt-3">Enviado por Matheus</p>
+                            <p className="w-96 justify-self-center mt-2">
+                                " Feita com morangos frescos e cachaça, essa variação doce e equilibrada da caipirinha tradicional é perfeita para festas ou momentos de relaxamento. Experimente e encante seus convidados! "
+                            </p>
+                        </div>
+                        <div className="flex gap-5 justify-center mt-8">
+                            <div className="relative flex flex-col justify-center items-center w-28 h-28 border-[1px] border-redwood shadow-lg rounded-2xl">
+                                <img className="absolute top-[-20px] w-12 h-12" src={Relogio} alt="" />
+                                <p className="uppercase font-bold mt-5">12 min</p>
+                                <p className="uppercase text-xs w-16 font-semibold">Tempo de Preparo</p>
+                            </div>
+                            <div className="relative flex flex-col justify-center items-center w-28 h-28 border-[1px] border-redwood shadow-lg rounded-2xl">
+                                <img className="absolute top-[-20px] w-12 h-12" src={Nivel} alt="" />
+                                <p className="uppercase font-bold mt-5">Facil</p>
+                                <p className="uppercase text-xs w-16 font-semibold">culdade</p>
+                            </div>
+                            <div className="relative flex flex-col justify-center items-center w-28 h-28 border-[1px] border-redwood shadow-lg rounded-2xl">
+                                <img className="absolute top-[-20px] w-12 h-12" src={Chefe} alt="" />
+                                <p className="uppercase font-bold mt-5">1 pessoa</p>
+                                <p className="uppercase text-xs w-16 font-semibold">Serve</p>
+                            </div>
+                            
+                        </div>
+
                     </div>
                 </div>
                 <div className="flex justify-between mr-3 mt-3">
