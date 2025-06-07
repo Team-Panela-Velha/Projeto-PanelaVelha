b76364de309679d6b774f5a6b1e260bb0e1d4836
JoYoneyama <jvsyoneyama@gmail.com>
2025-06-07 09:52:01 -0300

    responsividade de cadastro e login

--- Client/src/pages/Cadastro.jsx
+++ Client/src/pages/Cadastro.jsx
@@ -27,25 +27,27 @@ const Cadastro = () => {
     return (
 
         <div className="flex justify-center items-center w-full h-screen">
-            <form className="flex flex-col items-center justify-center gap-8 w-2/5 h-3/4 bg-slate-100 rounded-lg tela-login" onSubmit={enviarDados}>
+            <form className="flex flex-col items-center justify-center gap-8 w-4/5 sm:w-3/5 lg:w-2/5 h-3/4 bg-slate-100 rounded-lg tela-login" onSubmit={enviarDados}>
                 <h1 className="text-redwood font-bold text-3xl mb-4">CADASTRO</h1>
                 <div className="flex flex-col gap-4 w-full">
                     <div className="flex flex-col gap-1">
                         <label className="relative left-1/3" for="username">Usuário</label>
-                        <input className="w-1/3 relative left-1/3 h-7" type="text" id="username" name="username" value={nome} onChange={(e) => setNome(e.target.value)} required></input>
+                        <input className="w-1/3 mx-auto h-7" type="text" id="username" name="username" value={nome} onChange={(e) => setNome(e.target.value)} required></input>
                     </div>
                         
                     <div className="flex flex-col gap-1">
                         <label className="relative left-1/3" for="password">Senha</label>
-                        <input className="w-1/3 relative left-1/3 h-7" type="password" id="password" name="password" value={senha} onChange={(e) => setSenha(e.target.value)} required></input>
+                        <input className="w-1/3 mx-auto h-7" type="password" id="password" name="password" value={senha} onChange={(e) => setSenha(e.target.value)} required></input>
                     </div>
                 </div>
                 <div className="w-3/12">
-                    <div>
-                        <button className="bg-redwood w-full h-10 rounded-sm" type="submit"><p className="text-white">Cadastrar</p></button>
+                    <div className="flex justify-center">
+                        <button className="bg-redwood w-20 h-10 rounded-sm" type="submit"><p className="text-white">Cadastrar</p></button>
                     </div>
-                </div>    
-                <p className="relative right-[13%] top-10">Já possui uma conta? <Link to="/login" className="text-redwood">Faça login aqui</Link></p>         
+                </div> 
+                <div className="w-full mt-24">
+                    <p className="pl-[10%]">Já possui uma conta? <Link to="/login" className="text-redwood">Faça login aqui</Link></p>         
+                </div>   
             </form>
         </div>
 
