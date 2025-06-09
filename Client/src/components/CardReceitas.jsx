5e1b9aaf8753e38461c45f7e64b06841bc32e646
Mathlps <matheussouzalopes2006@gmail.com>
2025-06-09 19:32:20 -0300

    Receitaspopulares arrumado tablet

--- Client/src/components/CardReceitas.jsx
+++ Client/src/components/CardReceitas.jsx
@@ -5,7 +5,7 @@ const CardReceitas = ({ receita }) => {
   const { id, imagem_receita, nome_receita } = receita;
 
   return (
-    <div className="flex flex-col items-center ">
+    <div className="flex flex-col items-center shrink-0">
       <Link to={`/receitas/${id}`}>
       <div
         className="bg-orange-200 h-20 w-[110px] xs:w-[120px] sm:h-[130px] sm:w-[180px] lg:h-[120px] lg:w-[160px] rounded-md transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg"
@@ -16,7 +16,7 @@ const CardReceitas = ({ receita }) => {
         }}
       ></div></Link>
       <div className="pt-4 ">
-        <p className="flex flex-wrap justify-center w-[110px] xs:x-[120px] sm:w-[180px] lg:w-[160px] font-bold text-xs sm:text-sm leading-tight text-center first-letter:uppercase font-mono">
+        <p className="flex flex-wrap justify-center w-[110px] xs:w-[120px] sm:w-[180px] lg:w-[120px] font-bold text-xs sm:text-lg lg:text-sm leading-tight text-center first-letter:uppercase font-mono">
           <Link to={`/receitas/${id}`}>{nome_receita}</Link>
         </p>
       </div>
--- Client/src/components/MaisReceitas.jsx
+++ Client/src/components/MaisReceitas.jsx
@@ -5,29 +5,29 @@ import Card from "../components/CardReceitas"; // Importando o componente Card
 
 const MaisReceitas = () => {
 
-    const [receitas, setReceitas] = useState([]);
+  const [receitas, setReceitas] = useState([]);
 
-    async function fetchReceitas(){
-        axios.get("http://127.0.0.1:5000/api/mostrar_receitas_mais")
-        .then(response => {
-            setReceitas(response.data.receitas);
-        })
-        .catch(err => console.log(err))
-    };
+  async function fetchReceitas() {
+    axios.get("http://127.0.0.1:5000/api/mostrar_receitas_mais")
+      .then(response => {
+        setReceitas(response.data.receitas);
+      })
+      .catch(err => console.log(err))
+  };
 
-    useEffect(() => {
-        fetchReceitas();
-    }, []);
+  useEffect(() => {
+    fetchReceitas();
+  }, []);
 
-    return (
-       <div className="w-[98%] overflow-x-auto snap-x snap-mandatory bg-slate-100 rounded-md">
-      <div className="flex sm:justify-center gap-3 p-4 sm:ml-12 lg:ml-0">
+  return (
+    <div className="w-[98%] overflow-x-auto snap-x snap-mandatory bg-slate-100 rounded-md">
+      <div className="flex lg:justify-center gap-3 p-4 ">
         {receitas.map((receita) => (
           <Card key={receita.id} receita={receita} />
         ))}
       </div>
     </div>
-    );
+  );
 };
 
 export default MaisReceitas;
